77ce90c34a56614958bb104fba855a754092762c
VNcerti <tlvdzreal@gmail.com>
2025-12-07 13:10:12 +0700

    Add files via upload

---
+++ ioscert-main/appstore/js/config.js
@@ -0,0 +1,15 @@
+// Configuration variables
+const CONFIG = {
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbywHM36jEQpXFXzBTdG-EcbdbKWbtMyKeOSjNwUt-XahtstAmm1F_R2fr8JWUms_A1jsQ/exec',
+    CACHE_KEY: 'xspace_apps_cache',
+    CACHE_TIMESTAMP_KEY: 'xspace_cache_timestamp',
+    CACHE_DURATION: 30 * 60 * 1000, // 30 phút
+    CATEGORY_LABELS: {
+        'game': 'Trò chơi',
+        'social': 'Mạng xã hội',
+        'entertainment': 'Giải trí',
+        'photo': 'Chụp ảnh',
+        'clone': 'Nhân bản',
+        'premium': 'Mở khoá Premium'
+    }
+};
